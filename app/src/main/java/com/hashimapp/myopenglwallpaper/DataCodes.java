a192723915588321d921f91193b78a166078c821
blax3k <blax3k@gmail.com>
2016-03-02 18:25:40 -0800

    Build 0.010 Still lots to do

---
+++ app/src/main/java/com/hashimapp/myopenglwallpaper/DataCodes.java
@@ -0,0 +1,18 @@
+package com.hashimapp.myopenglwallpaper;
+
+/**
+ * Created by User on 3/2/2016.
+ */
+public class DataCodes {
+    public static int SKY = 000;
+    public static int CITY = 001;
+    public static int BUILDING = 002;
+    public static int ROOM = 003;
+    public static int GIRL = 004;
+    public static int TABLE = 005;
+    public static int CAT = 006;
+
+    public static int BLUR_NONE  = 20;
+    public static int BLUR_LOW = 21;
+    public static int BLUR_HIGH = 22;
+}
